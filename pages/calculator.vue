90053d8335a49acdf40963cfb2f23521978a8bc6
boazmatz <60620823+boazmatz@users.noreply.github.com>
2021-03-21 00:40:15 +0200

    build the calculator

---
+++ pages/calculator.vue
@@ -0,0 +1,8 @@
+<template src="../templates/calculator/calculator.html">
+</template>
+
+<script src="../templates/calculator/calculator.js">
+</script>
+
+<style src="../templates/calculator/calculator.css">
+</style>
