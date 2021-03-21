af01f9c37686328f684761467cf4ffb083a7a5e1
boazmatz <60620823+boazmatz@users.noreply.github.com>
2021-03-21 13:00:26 +0200

    back to work

--- pages/calculator.vue
+++ pages/calculator.vue
@@ -1,7 +1,31 @@
 <template src="../templates/calculator/calculator.html">
 </template>
 
-<script src="../templates/calculator/calculator.css">
+<script>
+export default {
+    methods: {
+          sumInputs() {
+            let totalOfExcersice = document.getElementById("totalOfExcercise")
+            let numberA = document.getElementById("numberA").value;
+            let numberB = document.getElementById("numberB").value;
+            let getNumA = Number(numberA)
+            let getNumB = Number(numberB)
+            let choosenOperator = this.select;
+            if (choosenOperator == 'Plus') {
+                totalOfExcersice.innerHTML = getNumA + getNumB;
+            }
+            else if (choosenOperator == 'Minus') {
+                totalOfExcersice.innerHTML = getNumA - getNumB;
+            } else if (choosenOperator == 'Multiplication') {
+                totalOfExcersice.innerHTML = getNumA * getNumB;
+            } else if (choosenOperator == 'Division') {
+                totalOfExcersice.innerHTML = getNumA / getNumB;
+            }
+        }
+
+    }
+
+}
 </script>
 
 <style src="../templates/calculator/calculator.css">
